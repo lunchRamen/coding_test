5d5143d756de38396ad111ba91c483a4662e59c2
lunchRamen <bloom6561@naver.com>
2021-06-24 12:07:16 +0900

    Create 내적.cpp

---
+++ Programmers/내적.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 1234567890;
+    int sum=0;
+    for(int i=0;i<a.size();i++){
+        sum+=a[i]*b[i];
+    }
+    answer=sum;
+    return answer;
+}
