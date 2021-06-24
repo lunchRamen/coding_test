20b3b2a450f78d622ab5f13cb8241a16334f03bb
lunchRamen <bloom6561@naver.com>
2021-06-24 13:57:11 +0900

    Create 하샤드 수.cpp

---
+++ Programmers/하샤드 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int temp=x;
+    int sum=0;
+    while(temp){
+        int res=temp%10;
+        sum+=res;
+        temp=temp/10;
+    }
+    if(x%sum==0) answer=true;
+    else answer=false;
+    return answer;
+}
