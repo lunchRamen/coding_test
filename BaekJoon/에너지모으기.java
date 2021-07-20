3849d829850a2bd8a9e4a7d8aebd8132a332bbbf
lunchRamen <bloom6561@naver.com>
2021-07-21 03:43:10 +0900

    Update 에너지모으기.java

--- BaekJoon/에너지모으기.java
+++ BaekJoon/에너지모으기.java
@@ -21,6 +21,45 @@ ex) 1 2 3 4
 
 객체로 만든 변수는 무조건 새로 객체 생성 후 인자로 넘기기.
 
+/*
+기본 자료형의 객체자료형도, 재귀 함수 호출시 모두 함께 업데이트 되니까 항상 새로 객체 생성 후 인자 넘기기
+또한, 객체를 삭제 수정해서 재귀 넘길때도 새로 객체 지정해서 삭제 수정 후 넘겨야 업데이트 되는걸 방지 해줄수 있다.
+객체의 경우 정의만하고 쓰지 않으면 NPE가 발생할 수 있다. 정의 후 선언도 같이 해주자.
+
+그리고 객체의 size로 for문을 돌릴때도, 항상 int형으로 받아서 for문을 돌리는 버릇 들이자.
+그리고 ArrayList의 객체 자료형의 기본자료형으로도 for문을 자동으로 돌릴수 있다.(unboxing)
+
+c++의 벡터를 ArrayList로 구현시
+1.선언의 경우
+vector<int> a=> ArrayList<Integer> a=new ArrayList<>();
+c++의경우 정의만하고 따로 선언해도 됐지만, 자바의 경우 정의와 선언을 같이 해줘야 NPE가 안난다.
+
+2.함수 호출
+void go(vector<int> a){
+a.push_back(1);
+go(a);
+}
+
+static void go(ArrayList<Integer> a){
+ArrayList<Integer> b=new ArrayList<>(a);
+b.add(1);
+go(b);
+}
+c++의 경우 vector자료구조를 그냥 그대로 쓰면 되지만, 자바의 경우 인자로 넘긴 자료구조가 객체를 원소로 가지는 경우
+항상 그전에 새로 정의해서 인자로 받은 자료구조를 복사하고 그걸 수정,삭제,복사해서 재귀 인자로 넘겨줘야 생각한대로 구현이 된다.
+
+void go(vector<int> &a){
+a.push_back(1);
+go(a);
+}
+static void go(ArrayList<Integer> a){
+a.add(1);
+go(a);
+}
+이렇게 인자로 넘긴 자료구조의 업데이트를 공유하고 싶은경우에는 오히려 자바가 더 편리할 수 있지만, PS의 경우 항상 업데이트
+해서 자료구조 넘기는게 중요하기 때문에 위의 경우로 생각하고 밑의 경우를 예외로 두고 생각하도록 하자.
+ */
+
  */
 
 import java.util.ArrayList;
